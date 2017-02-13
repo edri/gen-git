160fe9bb75b09115253dc89b1ccf86fc84db8994
miguelsantamaria <miguel@avalia.systems>
2017-02-13 18:23:28 +0100

    Update the Updated class and add a NewClass class.

---
+++ src/NewCreated.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Miguel on 13.02.2017.
+ */
+public class NewCreated {
+}
--- src/Updated.java
+++ src/Updated.java
@@ -2,4 +2,9 @@
  * Created by Miguel on 13.02.2017.
  */
 public class Updated {
+    private String myString;
+
+    public Updated(String myString) {
+        this.myString = myString;
+    }
 }
