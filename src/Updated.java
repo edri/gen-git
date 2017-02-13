1d8694ada7a9d05f23575825479884b8f9c09e5d
Miguel Santamaria <mig.santamar@gmail.com>
2017-02-13 18:24:23 +0100

    Create a conflict.

--- src/Updated.java
+++ src/Updated.java
@@ -2,7 +2,7 @@
  * Created by Miguel on 13.02.2017.
  */
 public class Updated {
-    private String myString;
+    private String myNewConflictedString;
 
     public Updated(String myString) {
         this.myString = myString;
