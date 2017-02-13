06ae9ee1d8d317393e7e2a7150a4d612aa4f59a4
miguelsantamaria <miguel@avalia.systems>
2017-02-13 18:25:18 +0100

    Update the Updated class' attributes.

--- src/Updated.java
+++ src/Updated.java
@@ -2,9 +2,10 @@
  * Created by Miguel on 13.02.2017.
  */
 public class Updated {
-    private String myString;
+    private String myNewString;
+    private int myInt;
 
     public Updated(String myString) {
-        this.myString = myString;
+        this.myNewString = myNewString;
     }
 }
